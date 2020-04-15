3e860b744f7a127c3a98b797481b562caff80d55
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2020-04-16 02:01:55 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -49,7 +49,7 @@ def ml_loop():
         # 3.3. Put the code here to handle the scene information
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
-            comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
+            comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_RIGHT)
             ball_served = True  
         else:
             if scene_info.ball[1]-ball_y>0: #ball is moving downward
