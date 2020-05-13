7be04cb7be4ebc8cf1c5251feb80e826cda14cee
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2020-05-14 00:02:47 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -27,14 +27,7 @@ def ml_loop(side: str):
         if player == '1P':
             if scene_info["platform_1P"][0]>pred-20:return 2
             elif scene_info["platform_1P"][0]<pred-20:return 1
-            else:
-                '''if scene_info["ball"][1]>390: #slicing
-                    if scene_info["ball_speed"][0]>0:
-                        return 1
-                    else:
-                        return 2
-                else:'''
-                return 0
+            else:return 0
                     
         else :
             if scene_info["platform_2P"][0]>pred-20:return 2
