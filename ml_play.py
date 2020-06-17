fd78f02752409ddce10b9aff9fc04e9d723d350a
YeeHsun <33437579+YeeHsun@users.noreply.github.com>
2020-06-17 21:26:30 +0800

    Update ml_play.py

--- ml_play.py
+++ ml_play.py
@@ -15,6 +15,8 @@ class MLPlay:
         self.lanes = [35, 105, 175, 245, 315, 385, 455, 525, 595]  # lanes center
         self.done = 0  
         self.init_lane = 1
+        self.change_lane = 0
+        self.command = 0
         pass
 
     def update(self, scene_info):
@@ -32,6 +34,7 @@ class MLPlay:
         def check_grid():
             grid = set()
             speed_ahead = 100
+            
             if self.car_pos[0] <= 35: # left bound//65
                 grid.add(1)
                 grid.add(4)
@@ -46,27 +49,42 @@ class MLPlay:
                     x = self.car_pos[0] - car["pos"][0] # x relative position
                     y = self.car_pos[1] - car["pos"][1] # y relative position
                     if x > -80 and x < -20 :
-                        if y > 80 and y < 120:
-                            grid.add(3)
+                        if y > 80 and y<250:
+                            grid.add(2)
+                            if y > 80 and y < 120:
+                                grid.add(3)
                         elif y < -80 and y > -200:
                             grid.add(9)
                         elif y < 80 and y > -80:
                             grid.add(6)
                     if x < 80 and x > 20:
-                        if y > 80 and y < 120:
-                            grid.add(1)
+                        if y>80 and y<250:
+                            grid.add(0)
+                            if y > 80 and y < 120:
+                                grid.add(1)
                         elif y < -80 and y > -200:
                             grid.add(7)
                         elif y < 80 and y > -80:
                             grid.add(4)
+            #if self.change_lane == 0:
             return move(grid= grid, speed_ahead = speed_ahead)
+            """else:
+                if self.command == 1:
+                    if self.car_pos[0]==self.init_lane-35:
+                        self.change_lane = 0
+                    return ["SPEED","MOVE_RIGHT"]
+                elif self.command == 2:
+                    if self.car_pos[0]==self.init_lane-35:
+                        self.change_lane = 0
+                    return ["SPEED","MOVE_LEFT"]"""
             
                 
                     
                 
             
         def move(grid, speed_ahead): 
-            if self.player_no == 0 or self.player_no == 1 or self.player_no == 2 or self.player_no == 3:
+            if self.player_no == 0 :
+                #print (self.car_pos[0])
                 print(grid)
                 '''print ("car lane:")
                 print (self.car_lane)
@@ -84,35 +102,47 @@ class MLPlay:
                 if self.done == 0:
                     self.init_lane=self.lanes[self.car_lane]
                     self.done = 1
-                if self.car_pos[0]>self.init_lane-35:
-                    return ["SPEED","MOVE_LEFT"]
-                elif self.car_pos[0]<self.init_lane-35:
-                    return ["SPEED","MOVE_RIGHT"]
-                else:
-                    return ["SPEED"]
+                return goto(destination=self.init_lane-35)
             else:
-                if (1 in grid) and (3 in grid):
-                    return ["BRAKE"]
+                '''if (0 in grid) and (2 in grid):
+                    if (1 not in grid) and (4 not in grid):
+                        if self.change_lane == 0:
+                            self.init_lane-=70
+                            self.change_lane = 1
+                        self.command = 1    
+                    elif (3 not in grid) and (6 not in grid):
+                        if self.change_lane == 0:
+                            self.init_lane+=70
+                            self.change_lane = 1
+                        self.command = 2'''
+                if (0 in grid) and (1 in grid) and (2 in grid) and (3 in grid):
+                    return ["NONE"]        
+                elif (0 in grid) and (3 in grid):
+                    return ["NONE"]
+                elif (1 in grid) and (2 in grid):
+                    return ["NONE"]
+                elif (1 in grid) and (3 in grid):
+                    return ["NONE"]
+                elif (0 in grid) and (2 in grid):
+                    return ["NONE"]
+                elif (0 in grid) and (1 in grid) and (6 in grid):
+                    return ["NONE"]
                 elif (1 in grid) :
-                    if self.car_pos[0]>self.init_lane-25:
-                        return ["SPEED","MOVE_LEFT"]
-                    elif self.car_pos[0]<self.init_lane-25:
-                        return ["SPEED","MOVE_RIGHT"]
-                    else:
-                        return ["SPEED"]
+                    return goto(destination=self.init_lane-25)
                 elif (3 in grid):
-                    if self.car_pos[0]>self.init_lane-45:
-                        return ["SPEED","MOVE_LEFT"]
-                    elif self.car_pos[0]<self.init_lane-45:
-                        return ["SPEED","MOVE_RIGHT"]
-                    else:
-                        return ["SPEED"]
-                
+                    return goto(destination=self.init_lane-45)
                 else:
                     return ["SPEED"]
                     
                                 
-                    
+        def goto(destination):
+            if self.car_pos[0]>destination:
+                return ["SPEED","MOVE_LEFT"]
+            elif self.car_pos[0]<destination:
+                return ["SPEED","MOVE_RIGHT"]
+            else:
+                return ["SPEED"]
+
         if len(scene_info[self.player]) != 0:
             self.car_pos = scene_info[self.player]
 
